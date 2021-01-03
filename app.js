5866551c2b4086da5ce54d88d62b717496da6358
Shyam Sundar Jha <ssjh360@gmail.com>
2021-01-03 13:31:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var outputDiv = document.querySelector("#output");
 
 var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
+
 function getTranslationURL(input) {
     return serverURL + "?" + "text=" + input
 }
